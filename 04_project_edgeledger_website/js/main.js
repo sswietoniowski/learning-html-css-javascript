9fee06a96c6a3bbf6352fb2d29ee78eddfb9b644
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-04-12 16:16:27 +0200

    Added smooth scrolling

--- 04_project_edgeledger_website/js/main.js
+++ 04_project_edgeledger_website/js/main.js
@@ -15,3 +15,28 @@ function initMap() {
   // The marker, positioned at location
   const marker = new google.maps.Marker({ position: loc, map: map });
 }
+
+// Sticky menu background
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
