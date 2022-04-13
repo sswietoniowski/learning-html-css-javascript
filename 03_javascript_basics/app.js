f069508ae549b5db48adeb12dd4a8a61f7616b0c
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-04-13 19:41:58 +0200

    Created starer code and used console

---
+++ 03_javascript_basics/app.js
@@ -0,0 +1,30 @@
+// alert('Hello world!');
+
+// Log to console
+console.clear();
+console.log('Hello world!');
+console.log(123);
+console.log(true);
+var greeting = 'Hello world!';
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+console.log({ a: 1, b: 2 });
+console.table({ a: 1, b: 2 });
+console.error('This is some error');
+console.warn('This is a warning');
+console.time('Hello');
+{
+  console.log('Hello World!');
+  console.log('Hello World!');
+  console.log('Hello World!');
+  console.log('Hello World!');
+  console.log('Hello World!');
+  console.log('Hello World!');
+}
+console.timeEnd('Hello');
+
+/* 
+
+Multiline comment
+
+*/
