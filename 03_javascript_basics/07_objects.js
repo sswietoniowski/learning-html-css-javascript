0f9c2ba413993549392a8ab9db6f6fdd1971e252
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-04-15 17:59:01 +0200

    Used basic object literals

--- 03_javascript_basics/07_objects.js
+++ 03_javascript_basics/07_objects.js
@@ -1,1 +1,41 @@
+// Basics of object literals
+const person = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 50,
+  hobbies: ['music', 'sports'],
+  address: {
+    street: '50 Main st',
+    city: 'Boston',
+    state: 'MA',
+  },
+  getBirthYear: function () {
+    return 2022 - this.age;
+  },
+};
 
+let val;
+
+val = person;
+
+console.log(val);
+console.log(val.firstName); // recommended
+console.log(val['firstName']);
+val.lastName = 'Smith';
+console.log(val);
+val.hobbies.push('food');
+console.log(val.hobbies[0]);
+console.log(val.address.city);
+console.log(val.getBirthYear());
+
+const people = [
+  { name: 'John Doe', age: 50 },
+  { name: 'Alice Smith', age: 20 },
+];
+
+console.log(people);
+console.table(people);
+
+for (const person of people) {
+  console.log(`Person: ${person.name}`);
+}
