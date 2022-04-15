d6fb461a6d402c1fad3b177eae0242c93d8e3392
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-04-15 10:59:26 +0200

    Added excerise description

--- 03_javascript_basics/05_strings.js
+++ 03_javascript_basics/05_strings.js
@@ -24,6 +24,25 @@ console.log(val.includes('John')); // tests whether a string contains another st
 // String interpolation
 console.log(`${firstName} ${lastName}`); // ES6, but compatible with all modern browsers
 
+/* 
+Have the function SymmetricTree(strArr) take the array of strings stored in strArr, which 
+will represent a binary tree, and determine if the tree is symmetric (a mirror image of itself). 
+The array will be implemented similar to how a binary heap is implemented, except the tree may 
+not be complete and NULL nodes on any level of the tree will be represented with a #. 
+
+For example: if strArr is ["1", "2", "2", "3", "#", "#", "3"] then this tree looks like the following:
+1
+/ \
+2   2
+/ \ / \
+3  # #  3
+For the input above, your program should return the string true because the binary tree is symmetric.
+Examples
+Input: new string[] {"4", "3", "4"}
+Output: false
+Input: new string[] {"10", "2", "2", "#", "1", "1", "#"}
+Output: true
+*/
 const isSymmetric = (strArr, start, end) => {
   let length = end - start + 1;
   for (let i = 0; i < Math.floor(length / 2); i++) {
