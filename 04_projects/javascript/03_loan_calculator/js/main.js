8f2b5b4078a09694b8cde27b053327a5f7006e6c
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-04-19 21:38:36 +0200

    Added loader & improved user experience

--- 04_projects/javascript/03_loan_calculator/js/main.js
+++ 04_projects/javascript/03_loan_calculator/js/main.js
@@ -1,9 +1,20 @@
 // Listen for submit
-document
-  .getElementById('loan-form')
-  .addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  // Stop form from submitting
+  e.preventDefault();
+});
 
 function calculateResults(e) {
+  console.log('Calculating...');
+
   // UI Vars
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
@@ -28,12 +39,16 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+
+    // Show results
+    document.getElementById('results').style.display = 'block';
+
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
   } else {
     // Show the error
     showError('Please check your numbers');
   }
-
-  e.preventDefault();
 }
 
 // Show error
