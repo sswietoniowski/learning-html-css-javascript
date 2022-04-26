cd9337c383d99b972e847364a43174fb9c0e34cb
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-04-26 13:14:21 +0200

    Added animation

---
+++ 04_projects/html_and_css/01_mini/01_presentation_website/js/app.js
@@ -0,0 +1,13 @@
+// Smooth Scrolling
+$('.btn').on('click', function (e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+    const hash = this.hash;
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800
+    );
+  }
+});
