0e3d8eb6e0c08d0177d6d5e5c30a62e2b78ef27e
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-05-17 20:42:20 +0200

    Limited number of the repos returned

--- 04_projects/javascript/06_github_finder/js/github.js
+++ 04_projects/javascript/06_github_finder/js/github.js
@@ -3,6 +3,8 @@ class GitHub {
     // define these properties to overcome GitHub API rate limit
     this.client_id = 'PUT_REAL_CLIENT_ID';
     this.client_secret = 'PUT_REAL_CLIENT_SECRET';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
@@ -12,7 +14,7 @@ class GitHub {
     const profile = await profileResponse.json();
 
     const reposResponse = await fetch(
-      `https://api.github.com/users/${user}/repos?per_page=100&client_id=${this.client_id}&client_secret=${this.client_secret}`
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
     const repos = await reposResponse.json();
 
