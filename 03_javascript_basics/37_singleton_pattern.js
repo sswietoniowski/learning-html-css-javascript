581205cbb69229de21e8fb2d35335aa2cd343661
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-05-31 11:39:04 +0200

    Implemented singleton pattern

---
+++ 03_javascript_basics/37_singleton_pattern.js
@@ -0,0 +1,25 @@
+// SINGLETON PATTERN
+
+const Singleton = (function () {
+  let instance;
+
+  function createInstance() {
+    const object = new Object('I am the instance');
+    return object;
+  }
+
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    },
+  };
+})();
+
+// Get a Singleton instance
+const instance1 = Singleton.getInstance();
+console.log(instance1);
+const instance2 = Singleton.getInstance();
+console.log(instance2);
