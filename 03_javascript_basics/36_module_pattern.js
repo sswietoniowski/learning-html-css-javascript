399fde7efde9e4ac6457ecc9932d5d005709cb60
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-05-31 09:55:11 +0200

    Used module pattern

---
+++ 03_javascript_basics/36_module_pattern.js
@@ -0,0 +1,58 @@
+// MODULE PATTERN
+
+// Basic structure of the module pattern
+
+// (function() {
+//     // Declare private vars and functions
+
+//     return {
+//         // Declare public vars and functions
+//     }
+// })();
+
+const UIController = (function () {
+  let text = 'Hello World!';
+
+  const changeText = function () {
+    const element = document.getElementById('heading');
+    element.textContent = text;
+  };
+
+  return {
+    callChangeText: function () {
+      changeText();
+      console.log(text);
+    },
+  };
+})();
+
+UIController.callChangeText();
+
+// UIController.changeText();
+// console.log(UIController.text);
+
+// REVEALING MODULE PATTERN
+
+const ItemController = (function () {
+  let _data = [];
+
+  function add(item) {
+    _data.push(item);
+    console.log('Item Added...');
+  }
+
+  function get(id) {
+    return _data.find((item) => item.id === id);
+  }
+
+  return {
+    add: add,
+    get: get,
+  };
+})();
+
+ItemController.add({ id: 1, name: 'John' });
+ItemController.add({ id: 2, name: 'Peter' });
+ItemController.add({ id: 3, name: 'Mike' });
+
+console.log(ItemController.get(2));
