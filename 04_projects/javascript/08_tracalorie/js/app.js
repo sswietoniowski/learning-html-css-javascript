67f30ce3d116fb8bb06db45f6ca4a9abe79be068
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-06-07 18:11:58 +0200

    Retrieved and populated items

--- 04_projects/javascript/08_tracalorie/js/app.js
+++ 04_projects/javascript/08_tracalorie/js/app.js
@@ -27,16 +27,47 @@ const ItemController = (function () {
 
   // Public methods
   return {
-    logData: function () {
-      return data;
+    getItems: function () {
+      return data.items;
     },
   };
 })();
 
 // UI Controller
 const UIController = (function () {
+  const UISelectors = {
+    itemList: '#item-list',
+    listItems: '#item-list li',
+    addBtn: '.add-btn',
+    updateBtn: '.update-btn',
+    deleteBtn: '.delete-btn',
+    backBtn: '.back-btn',
+    clearBtn: '.clear-btn',
+    itemNameInput: '#item-name',
+    itemCaloriesInput: '#item-calories',
+    totalCalories: '.total-calories',
+  };
+
   // Public methods
-  return {};
+  return {
+    populateItemList: function (items) {
+      let html = '';
+
+      items.forEach((item) => {
+        html += `
+          <li class="collection-item" id="item-${item.id}">
+            <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+            <a href="#" class="secondary-content">
+              <i class="edit-item fa fa-pencil"></i>
+            </a>
+          </li>
+        `;
+      });
+
+      // Insert list items
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    },
+  };
 })();
 
 // App
@@ -44,7 +75,11 @@ const App = (function (ItemController, UIController, StorageController) {
   // Public methods
   return {
     init: function () {
-      console.log(ItemController.logData());
+      // Fetches items from data structure
+      const items = ItemController.getItems();
+
+      // Populates list with items
+      UIController.populateItemList(items);
     },
   };
 })(ItemController, UIController, StorageController);
