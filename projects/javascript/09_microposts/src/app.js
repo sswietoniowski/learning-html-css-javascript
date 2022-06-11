89fff51ed6f7e32a921f7ecde141ecaea7a8b968
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-06-11 18:14:02 +0200

    Added posts & showed alerts

--- projects/javascript/09_microposts/src/app.js
+++ projects/javascript/09_microposts/src/app.js
@@ -6,6 +6,9 @@ import { ui } from './ui';
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', addPost);
+
 function getPosts() {
   http
     .get('http://localhost:3000/posts')
@@ -13,4 +16,40 @@ function getPosts() {
       ui.showPosts(data);
     })
     .catch((err) => console.log(err));
+}
+
+function addPost(e) {
+  e.preventDefault();
+
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const post = {
+    title,
+    body,
+  };
+
+  if (title === '' || body === '') {
+    ui.showAlert('Please fill in all fields', 'error');
+  } else {
+    if (ui.forState === 'add') {
+      http
+        .post('http://localhost:3000/posts', post)
+        .then((data) => {
+          ui.showAlert('Post added', 'success');
+          ui.clearFields();
+          getPosts();
+        })
+        .catch((err) => console.log(err));
+    } else {
+      http
+        .put(`http://localhost:3000/posts/${ui.id}`, post)
+        .then((data) => {
+          ui.showAlert('Post updated', 'success');
+          ui.clearFields();
+          getPosts();
+        })
+        .catch((err) => console.log(err));
+    }
+  }
 }
--- projects/javascript/09_microposts/src/ui.js
+++ projects/javascript/09_microposts/src/ui.js
@@ -8,6 +8,43 @@ class UI {
     this.forState = 'add';
   }
 
+  // Show alert
+  showAlert(message, className) {
+    // Create div
+    const div = document.createElement('div');
+    // Add class
+    div.className = `alert alert-${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.posts-container');
+    // Get posts
+    const posts = document.querySelector('#posts');
+    // Insert alert
+    container.insertBefore(div, posts);
+    // Timeout after 3 seconds
+    setTimeout(() => {
+      document.querySelector('.alert').remove();
+    }, 3000);
+  }
+
+  // Clear fields
+  clearFields() {
+    this.title.value = '';
+    this.body.value = '';
+  }
+
+  // Add a new post
+  addPost(post) {
+    const div = document.createElement('div');
+    div.className = 'post-item';
+    div.innerHTML = `<h3>${post.title}</h3>
+        <p>${post.body}</p>
+        <a href="#" class="edit-post">Edit</a>
+        <a href="#" class="delete-post">Delete</a>`;
+    this.posts.appendChild(div);
+  }
+
   // Show posts
   showPosts(posts) {
     let output = '';
