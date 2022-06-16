b512b199b009d1799e5cc35a34fe8b31a6a7e7fc
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-06-16 20:23:27 +0200

    Created contact page

--- projects/html_and_css/05_portfolio/dist/js/typewriter.js
+++ projects/html_and_css/05_portfolio/dist/js/typewriter.js
@@ -58,8 +58,10 @@ document.addEventListener('DOMContentLoaded', init);
 // Init App
 function init() {
   const txtElement = document.querySelector('.txt-type');
-  const words = JSON.parse(txtElement.getAttribute('data-words'));
-  const wait = txtElement.getAttribute('data-wait');
-  // Init TypeWriter
-  new TypeWriter(txtElement, words, wait);
+  if (txtElement !== null) {
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+    // Init TypeWriter
+    new TypeWriter(txtElement, words, wait);
+  }
 }
