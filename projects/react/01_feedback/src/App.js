d5e0dcea533797e9ef3be301b5029d8ee030b326
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-07-07 20:14:32 +0200

    Worked with className and htmlFor

--- projects/react/01_feedback/src/App.js
+++ projects/react/01_feedback/src/App.js
@@ -1,14 +1,17 @@
 function App() {
   return (
     // <> is a fragment, we could use <div> instead, but there must be one parent element
-    <>
+    // we're using className instead class because class is a reserved word in javascript
+    // we're using htmlFor instead for because it's a reserved word in javascript
+    <div className='container'>
       <h1>Hello from the app component</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit,
         voluptatem. Tempore dicta distinctio fuga iure fugit voluptas ipsa
         accusantium non.
       </p>
-    </>
+      <label htmlFor=''></label>
+    </div>
   );
 }
 
