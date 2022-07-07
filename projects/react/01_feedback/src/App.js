b061d4c01753da84cdb96ef7d40edd4b23ae5419
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-07-07 20:11:52 +0200

    Used fragment

--- projects/react/01_feedback/src/App.js
+++ projects/react/01_feedback/src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <h1>Hello from the app component</h1>;
+  return (
+    // <> is a fragment, we could use <div> instead, but there must be one parent element
+    <>
+      <h1>Hello from the app component</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit,
+        voluptatem. Tempore dicta distinctio fuga iure fugit voluptas ipsa
+        accusantium non.
+      </p>
+    </>
+  );
 }
 
 export default App;
