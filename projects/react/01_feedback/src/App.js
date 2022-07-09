52a4c1c5eae106337343f34c9c8c2f3fa3a20ee8
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-07-09 09:24:48 +0200

    Restored compnent to its start state

--- projects/react/01_feedback/src/App.js
+++ projects/react/01_feedback/src/App.js
@@ -1,47 +1,9 @@
 import React from 'react';
 
 function App() {
-  const title = 'Blog Post';
-  const body = 'My blog post body';
-  const comments = [
-    { id: 1, author: 'Bobby', text: 'This is my first comment' },
-    { id: 2, author: 'Sally', text: 'This is my second comment' },
-    { id: 3, author: 'Sue', text: 'This is my third comment' },
-  ];
-
-  const loading = false;
-
-  if (loading) {
-    return <h1>Loading...</h1>;
-  }
-
-  const showComments = true;
-
-  const commentsBlock = (
-    <div className='comments'>
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={comment.id}>
-            <p>{comment.text}</p>
-            <p>{comment.author}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-
   return (
-    // using dynamic values in JSX
-    // using lists in JSX
-    // we're using key instead of id
     <div className='container'>
-      <h1>{title}</h1>
-      <p>{body}</p>
-      <label htmlFor=''></label>
-      {Math.random() * (5 + 5)}
-      {showComments ? commentsBlock : null}
-      {showComments && commentsBlock}
+      <h1>My App</h1>
     </div>
   );
 }
