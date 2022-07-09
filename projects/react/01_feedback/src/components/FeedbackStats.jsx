497f4174712561ae841a223fdba4aac07152c71e
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-07-09 18:28:02 +0200

    Corrected how feedback is calculated

--- projects/react/01_feedback/src/components/FeedbackStats.jsx
+++ projects/react/01_feedback/src/components/FeedbackStats.jsx
@@ -8,10 +8,12 @@ function FeedbackStats({ feedback }) {
         }, 0) / feedback.length
       : 0;
 
+  average = average.toFixed(1).replace(/[.,]0*$/, '');
+
   return feedback.length > 0 ? (
     <div className='feedback-stats'>
       <h4>{feedback.length} Reviews</h4>
-      <h4>Average Rating: {average}</h4>
+      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
     </div>
   ) : null;
 }
