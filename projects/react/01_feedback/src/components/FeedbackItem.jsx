1db0410b41471882d8aca53a3dc002520ecc6d6e
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-07-09 17:17:49 +0200

    Created Card component and used it for styling

--- projects/react/01_feedback/src/components/FeedbackItem.jsx
+++ projects/react/01_feedback/src/components/FeedbackItem.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+
+import Card from './shared/Card';
 
 function FeedbackItem({ item }) {
   return (
-    <div className='card'>
+    <Card>
       <div className='num-display'>{item.rating}</div>
       <div className='text-display'>{item.text}</div>
-    </div>
+    </Card>
   );
 }
+
+FeedbackItem.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    rating: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default FeedbackItem;
