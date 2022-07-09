4377c48a9086836a18b718a5a03db0847fe4c4ef
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-07-09 18:17:04 +0200

    Added deletion confirmation

--- projects/react/01_feedback/src/App.jsx
+++ projects/react/01_feedback/src/App.jsx
@@ -6,15 +6,17 @@ import FeedbackList from './components/FeedbackList';
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
-  const handleDelete = (id) => {
-    setFeedback(feedback.filter((item) => item.id !== id));
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete this feedback?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
   };
 
   return (
     <>
       <Header />
       <div className='container'>
-        <FeedbackList feedback={feedback} handleDelete={handleDelete} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
