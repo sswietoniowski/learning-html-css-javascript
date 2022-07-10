b8b90c64e6584b413e9397d0fb6a5ef31e2c205e
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-07-10 21:43:05 +0200

    Added real-time validation

--- projects/react/01_feedback/src/components/FeedbackForm.jsx
+++ projects/react/01_feedback/src/components/FeedbackForm.jsx
@@ -5,8 +5,21 @@ import Button from './shared/Button';
 
 function FeedbackForm() {
   const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
   const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage('Feedback must be at least 10 characters long.');
+    } else {
+      setBtnDisabled(false);
+      setMessage(null);
+    }
+
     setText(e.target.value);
   };
 
@@ -22,8 +35,11 @@ function FeedbackForm() {
             placeholder='Write a review'
             value={text}
           />
-          <Button type='submit'>Send</Button>
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   );
