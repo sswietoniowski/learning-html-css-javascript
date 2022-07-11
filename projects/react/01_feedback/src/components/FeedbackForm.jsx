6e55d1d645ec1078970530e123c23cadb48cdff0
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-07-11 12:19:02 +0200

    Crated RatingSelect component

--- projects/react/01_feedback/src/components/FeedbackForm.jsx
+++ projects/react/01_feedback/src/components/FeedbackForm.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 
 import Card from './shared/Card';
 import Button from './shared/Button';
+import RatingSelect from './RatingSelect';
 
 function FeedbackForm() {
   const [text, setText] = useState('');
+  const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState('');
 
@@ -27,7 +29,7 @@ function FeedbackForm() {
     <Card>
       <form>
         <h2>How would your rate your service with us?</h2>
-        {/* @todo - rating select component */}
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className='input-group'>
           <input
             onChange={handleTextChange}
