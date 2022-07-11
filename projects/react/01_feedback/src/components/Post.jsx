28f24a9e666c584e7ffe1af080c1794deb47d5c2
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-07-11 16:55:32 +0200

    Tested Navigate and useNavigate

--- projects/react/01_feedback/src/components/Post.jsx
+++ projects/react/01_feedback/src/components/Post.jsx
@@ -1,12 +1,23 @@
-import { useParams } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 function Post() {
-  const params = useParams();
+  const status = 200;
+
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    console.log('Hello :-)');
+    navigate('/about');
+  };
+
+  if (status === 404) {
+    return <Navigate to='/notfound' />;
+  }
 
   return (
     <div>
-      <h1>Post: {params.id}</h1>
-      <p>Name: {params.name}</p>
+      <h1>Post</h1>
+      <button onClick={onClick}>Click</button>
     </div>
   );
 }
