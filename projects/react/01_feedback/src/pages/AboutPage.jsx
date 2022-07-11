d56d2bc5b55f113f1e1b51684bb9af1547f2ca3e
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-07-11 16:12:31 +0200

    Used different way of creating links

--- projects/react/01_feedback/src/pages/AboutPage.jsx
+++ projects/react/01_feedback/src/pages/AboutPage.jsx
@@ -10,7 +10,14 @@ function AboutPage() {
         <p>This is a React app to leave feedback for a product or service.</p>
         <p>Version: 1.0.0</p>
         <p>
-          <Link to='/'>Back To Home</Link>
+          <Link
+            to={{
+              pathname: '/',
+              search: '?sort=desc',
+            }}
+          >
+            Back To Home
+          </Link>
         </p>
       </div>
     </Card>
