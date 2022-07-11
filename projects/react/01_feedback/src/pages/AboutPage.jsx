ff750a0a0ff8c6581cee71aa74bf9c7c90a52ab1
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-07-11 16:13:12 +0200

    Another syntax

--- projects/react/01_feedback/src/pages/AboutPage.jsx
+++ projects/react/01_feedback/src/pages/AboutPage.jsx
@@ -14,6 +14,7 @@ function AboutPage() {
             to={{
               pathname: '/',
               search: '?sort=desc',
+              hash: '#about',
             }}
           >
             Back To Home
