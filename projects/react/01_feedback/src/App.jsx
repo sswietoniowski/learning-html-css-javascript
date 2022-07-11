2b813eb5c3f59c29c7f452483c5fd7d14f2bc431
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-07-11 16:08:22 +0200

    Created links (with page refresh)

--- projects/react/01_feedback/src/App.jsx
+++ projects/react/01_feedback/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutIconLink from './components/AboutIconLink';
 
 import AboutPage from './pages/AboutPage';
 
@@ -48,6 +49,7 @@ function App() {
           ></Route>
           <Route path='/about' element={<AboutPage />} />
         </Routes>
+        <AboutIconLink />
       </div>
     </Router>
   );
---
+++ projects/react/01_feedback/src/components/AboutIconLink.jsx
@@ -0,0 +1,13 @@
+import { FaQuestion } from 'react-icons/fa';
+
+function AboutIconLink() {
+  return (
+    <div className='about-link'>
+      <a href='/about'>
+        <FaQuestion size={30} />
+      </a>
+    </div>
+  );
+}
+
+export default AboutIconLink;
