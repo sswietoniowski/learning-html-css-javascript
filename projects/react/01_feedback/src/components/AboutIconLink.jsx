7fa0c8c1cc426538170c5b1cbf8a252f409e1367
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-07-11 16:10:46 +0200

    Used Link component

--- projects/react/01_feedback/src/components/AboutIconLink.jsx
+++ projects/react/01_feedback/src/components/AboutIconLink.jsx
@@ -1,11 +1,12 @@
 import { FaQuestion } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 function AboutIconLink() {
   return (
     <div className='about-link'>
-      <a href='/about'>
+      <Link to='/about'>
         <FaQuestion size={30} />
-      </a>
+      </Link>
     </div>
   );
 }
--- projects/react/01_feedback/src/pages/AboutPage.jsx
+++ projects/react/01_feedback/src/pages/AboutPage.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import Card from '../components/shared/Card';
 
 function AboutPage() {
@@ -8,7 +10,7 @@ function AboutPage() {
         <p>This is a React app to leave feedback for a product or service.</p>
         <p>Version: 1.0.0</p>
         <p>
-          <a href='/'>Back To Home</a>
+          <Link to='/'>Back To Home</Link>
         </p>
       </div>
     </Card>
