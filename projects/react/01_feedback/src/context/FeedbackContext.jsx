04a187b6e78286cca7529b10d9a9b61f1c75a964
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-07-12 21:11:47 +0200

    Moved test feedback data into context

--- projects/react/01_feedback/src/context/FeedbackContext.jsx
+++ projects/react/01_feedback/src/context/FeedbackContext.jsx
@@ -8,8 +8,18 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: '1',
-      text: 'This item is from context',
       rating: 10,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: '2',
+      rating: 9,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: '3',
+      rating: 8,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
     },
   ]);
 
