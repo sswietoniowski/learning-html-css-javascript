26416fb87293ff0fd0fe18968bec5eeaf94b9c65
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-07-13 11:06:52 +0200

    Final ? refactor

--- projects/react/01_feedback/src/context/FeedbackContext.jsx
+++ projects/react/01_feedback/src/context/FeedbackContext.jsx
@@ -67,7 +67,9 @@ export const FeedbackProvider = ({ children }) => {
     });
     const data = await response.json();
 
-    setFeedback(feedback.map((item) => (item.id === id ? data : item)));
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
+    );
   };
 
   return (
