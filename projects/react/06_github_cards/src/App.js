4b10246337a099700c88baef6fe3eb556ad90452
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-08-10 09:26:00 +0200

    Tested JS styling

--- projects/react/06_github_cards/src/App.js
+++ projects/react/06_github_cards/src/App.js
@@ -4,10 +4,15 @@ import { Component } from 'react';
 class Card extends Component {
   render() {
     return (
-      <div className='github-profile'>
+      <div className='github-profile' style={{ margin: '1rem' }}>
         <img src='https://placehold.it/75' alt='' />
-        <div className='info'>
-          <div className='name'>Name here</div>
+        <div
+          className='info'
+          style={{ display: 'inline-block', marginLef: 10 }}
+        >
+          <div className='name' style={{ fontSize: '125%' }}>
+            Name here
+          </div>
           <div className='company'>Company here</div>
         </div>
       </div>
