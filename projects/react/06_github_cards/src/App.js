e155cdb62165def53b24857966db0b6e5d70366e
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-08-10 09:36:13 +0200

    Fixed ID warning for lists

--- projects/react/06_github_cards/src/App.js
+++ projects/react/06_github_cards/src/App.js
@@ -3,16 +3,19 @@ import { Component } from 'react';
 
 const testData = [
   {
+    id: 1,
     name: 'Dan Abramov',
     avatar_url: 'https://avatars0.githubusercontent.com/u/810438?v=4',
     company: '@facebook',
   },
   {
+    id: 2,
     name: 'Sophie Alpert',
     avatar_url: 'https://avatars2.githubusercontent.com/u/6820?v=4',
     company: 'Humu',
   },
   {
+    id: 3,
     name: 'Sebastian Markbåge',
     avatar_url: 'https://avatars2.githubusercontent.com/u/63648?v=4',
     company: 'Facebook',
