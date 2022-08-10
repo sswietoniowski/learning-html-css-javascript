1b3de3ab865c4fee5bfc99216d1fab43778ea4b8
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-08-10 10:16:11 +0200

    Started with empty profiles

--- projects/react/06_github_cards/src/App.js
+++ projects/react/06_github_cards/src/App.js
@@ -2,26 +2,7 @@ import './App.css';
 import { Component } from 'react';
 import axios from 'axios';
 
-const testData = [
-  {
-    id: 1,
-    name: 'Dan Abramov',
-    avatar_url: 'https://avatars0.githubusercontent.com/u/810438?v=4',
-    company: '@facebook',
-  },
-  {
-    id: 2,
-    name: 'Sophie Alpert',
-    avatar_url: 'https://avatars2.githubusercontent.com/u/6820?v=4',
-    company: 'Humu',
-  },
-  {
-    id: 3,
-    name: 'Sebastian Markbåge',
-    avatar_url: 'https://avatars2.githubusercontent.com/u/63648?v=4',
-    company: 'Facebook',
-  },
-];
+const testData = [];
 
 const CardList = (props) => (
   <div>
