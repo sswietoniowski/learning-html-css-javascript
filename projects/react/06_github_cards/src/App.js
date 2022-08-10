2496ba9e0a8409ec44584f49d81a02ee05825862
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-08-10 08:34:14 +0200

    Prepared project base

--- projects/react/06_github_cards/src/App.js
+++ projects/react/06_github_cards/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Card extends Component {
+  render() {
+    return (
+      <div className='github-profile'>
+        <img src='https://placehold.it/75' alt='' />
+        <div className='info'>
+          <div className='name'>Name here</div>
+          <div className='company'>Company here</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+// const App = ({ title }) => <div className='header'>{title}</div>;
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className='header'>{this.props.title}</div>
+        <Card />
+      </div>
+    );
+  }
 }
 
 export default App;
