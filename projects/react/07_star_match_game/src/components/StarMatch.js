1292accc88c15e74a16e73f65c55548a51912a45
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-08-12 12:45:50 +0200

    Used static markup

--- projects/react/07_star_match_game/src/components/StarMatch.js
+++ projects/react/07_star_match_game/src/components/StarMatch.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import './StarMatch.css';
 
 // Color Theme
@@ -39,6 +41,7 @@ const utils = {
 };
 
 const StarMatch = () => {
+  const [stars, setStars] = useState(utils.random(1, 9));
   return (
     <div className='game'>
       <div className='help'>
@@ -46,26 +49,16 @@ const StarMatch = () => {
       </div>
       <div className='body'>
         <div className='left'>
-          <div className='star' />
-          <div className='star' />
-          <div className='star' />
-          <div className='star' />
-          <div className='star' />
-          <div className='star' />
-          <div className='star' />
-          <div className='star' />
-          <div className='star' />
+          {utils.range(1, stars).map((starId) => (
+            <div key={starId} className='star' />
+          ))}
         </div>
         <div className='right'>
-          <button className='number'>1</button>
-          <button className='number'>2</button>
-          <button className='number'>3</button>
-          <button className='number'>4</button>
-          <button className='number'>5</button>
-          <button className='number'>6</button>
-          <button className='number'>7</button>
-          <button className='number'>8</button>
-          <button className='number'>9</button>
+          {utils.range(1, stars).map((numberId) => (
+            <button key={numberId} className='number'>
+              {numberId}
+            </button>
+          ))}
         </div>
       </div>
       <div className='timer'>Time Remaining: 10</div>
