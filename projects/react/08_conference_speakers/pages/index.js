b19c6c38719d49dd9afec1a16473a478a7eb3a78
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-08-13 16:50:13 +0200

    Created a nested tree of components

--- projects/react/08_conference_speakers/pages/index.js
+++ projects/react/08_conference_speakers/pages/index.js
@@ -16,35 +16,46 @@ const Sessions = ({ sessions }) => {
   );
 };
 
-const Speaker = ({ speaker }) => {
-  const { id, bio, first, last, favorite, twitterHandle, company, sessions } =
-    speaker;
+const SpeakerImage = (props) => {
+  const { id, first, last } = props;
+  return (
+    <div className='speaker-img d-flex flex-row justify-content-center align-items-center h-300'>
+      <img
+        className='contain-fit'
+        src={`/images/speaker-${id}.jpg`}
+        width='300'
+        alt={`${first} ${last}`}
+      />
+    </div>
+  );
+};
 
+const SpeakerInfo = (props) => {
+  const { first, last, bio, company, twitterHandle, favorite } = props;
+  return (
+    <div className='speaker-info'>
+      <div className='d-flex justify-content-between mb-3'>
+        <h3 className='text-truncate w-200'>
+          {first} {last}
+        </h3>
+      </div>
+      <div>
+        <p>
+          {bio} {company} {twitterHandle} {favorite}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const Speaker = ({ speaker }) => {
   return (
     <div className='col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12'>
       <div className='card card-height p-4 mt-4'>
-        <div className='speaker-img d-flex flex-row justify-content-center align-items-center h-300'>
-          <img
-            className='contain-fit'
-            src={`/images/speaker-${id}.jpg`}
-            width='300'
-            alt={`${first} ${last}`}
-          />
-        </div>
-        <div className='speaker-info'>
-          <div className='d-flex justify-content-between mb-3'>
-            <h3 className='text-truncate w-200'>
-              {first} {last}
-            </h3>
-          </div>
-          <div>
-            <p>
-              {bio} {company} {twitterHandle} {favorite}
-            </p>
-          </div>
-        </div>
+        <SpeakerImage {...speaker} />
+        <SpeakerInfo {...speaker} />
       </div>
-      <Sessions sessions={sessions} />
+      <Sessions sessions={speaker.sessions} />
     </div>
   );
 };
