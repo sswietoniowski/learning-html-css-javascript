76289064951bb2aaf8fd8c3da4377934da53587b
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-08-13 16:58:11 +0200

    Extracted component to the separate file

---
+++ projects/react/08_conference_speakers/src/components/Speaker.js
@@ -0,0 +1,67 @@
+const Session = ({ title, room }) => {
+  return (
+    <span className='session w-100'>
+      {title} <strong>Room: {room}</strong>
+    </span>
+  );
+};
+
+const Sessions = ({ sessions }) => {
+  return (
+    <div className='sessionBox card h-250'>
+      <Session title={sessions[0].title} room={sessions[0].room.name} />
+    </div>
+  );
+};
+
+const SpeakerImage = ({ id, first, last }) => {
+  return (
+    <div className='speaker-img d-flex flex-row justify-content-center align-items-center h-300'>
+      <img
+        className='contain-fit'
+        src={`/images/speaker-${id}.jpg`}
+        width='300'
+        alt={`${first} ${last}`}
+      />
+    </div>
+  );
+};
+
+const SpeakerDemographics = ({
+  first,
+  last,
+  bio,
+  company,
+  twitterHandle,
+  favorite,
+}) => {
+  return (
+    <div className='speaker-info'>
+      <div className='d-flex justify-content-between mb-3'>
+        <h3 className='text-truncate w-200'>
+          {first} {last}
+        </h3>
+      </div>
+      <div>
+        <p>
+          {bio} {company} {twitterHandle} {favorite}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const Speaker = ({ speaker }) => {
+  const { id, first, last, bio, company, twitterHandle, favorite } = speaker;
+  return (
+    <div className='col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12'>
+      <div className='card card-height p-4 mt-4'>
+        <SpeakerImage id={id} first={first} last={last} />
+        <SpeakerDemographics {...speaker} />
+      </div>
+      <Sessions sessions={speaker.sessions} />
+    </div>
+  );
+};
+
+export default Speaker;
