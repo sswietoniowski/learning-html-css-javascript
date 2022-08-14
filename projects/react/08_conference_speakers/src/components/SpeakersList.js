16c7e859407258718d5d5e1cee76eb8a11769e7d
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-08-14 09:32:40 +0200

    Extracted hook to the separate file

--- projects/react/08_conference_speakers/src/components/SpeakersList.js
+++ projects/react/08_conference_speakers/src/components/SpeakersList.js
@@ -1,59 +1,7 @@
-import { useState, useEffect } from 'react';
 import Spinner from 'react-bootstrap/Spinner';
 
 import Speaker from './Speaker';
-import { data } from '../../SpeakerData';
-
-const useRequestSpeakers = (ms) => {
-  const [speakersData, setSpeakersData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [hasErrored, setHasErrored] = useState(false);
-  const [error, setError] = useState('');
-
-  const delay = (ms) => {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  };
-
-  useEffect(() => {
-    const delayFunc = async (ms) => {
-      try {
-        await delay(ms);
-        // throw 'Had error!';
-        setSpeakersData(data);
-        setIsLoading(false);
-      } catch (error) {
-        setIsLoading(false);
-        setHasErrored(true);
-        setError(error);
-      }
-    };
-
-    delayFunc(ms);
-  }, []);
-
-  const onFavoriteToggle = (id) => {
-    const speakerRecPrevious = speakersData.find(function (rec) {
-      return rec.id === id;
-    });
-    const speakerRecUpdated = {
-      ...speakerRecPrevious,
-      favorite: !speakerRecPrevious.favorite,
-    };
-    const speakersDataNew = speakersData.map(function (rec) {
-      return rec.id === id ? speakerRecUpdated : rec;
-    });
-
-    setSpeakersData(speakersDataNew);
-  };
-
-  return {
-    speakersData,
-    isLoading,
-    hasErrored,
-    error,
-    onFavoriteToggle,
-  };
-};
+import useRequestSpeakers from './hooks/useRequestSpeakers';
 
 const SpeakersList = ({ showSessions }) => {
   const { speakersData, isLoading, hasErrored, error, onFavoriteToggle } =
