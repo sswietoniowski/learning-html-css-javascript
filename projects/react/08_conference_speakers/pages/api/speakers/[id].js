3bcdb4d7d616f2928d1e4c68d175e6fc0192c195
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-22 22:03:03 +0200

    Fixed API call

--- projects/react/08_conference_speakers/pages/api/speakers/[id].js
+++ projects/react/08_conference_speakers/pages/api/speakers/[id].js
@@ -4,120 +4,119 @@ import fs from 'fs';
 const { promisify } = require('util');
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
-const delay = (ms) =>
-  new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export default async function handler(req, res) {
   const method = req?.method;
   const id = parseInt(req?.query.id);
   const recordFromBody = req?.body;
-
   const jsonFile = path.resolve('./', 'db.json');
-
-  const postMethod = async () => {
-    try {
-      const data = await readFile(jsonFile, 'utf8');
-      await delay(1000);
-      const speakers = JSON.parse(data).speakers;
-      if (speakers) {
-        const newSpeaker = {
-          id: speakers.length + 1,
-          ...recordFromBody,
-        };
-        speakers.push(newSpeaker);
-        await writeFile(jsonFile, JSON.stringify({ speakers }));
-        res.setHeader('Content-Type', 'application/json');
-        res.status(201).send(JSON.stringify(newSpeaker, null, 2));
-        console.log(`POST /api/speakers/${id} status: 201`);
-      } else {
-        res.status(404).send('Not found');
-        console.log(`POST /api/speakers/${id} status: 404`);
-      }
-    } catch (err) {
-      res.status(500).send(err);
-      console.log(`POST /api/speakers/${id} status: 500`);
-      console.log(`Error: ${err}`);
-    }
-  };
-
-  const putMethod = async () => {
-    try {
-      const data = await readFile(jsonFile, 'utf8');
-      await delay(1000);
-      const speakers = JSON.parse(data).speakers;
-      if (speakers) {
-        const speaker = speakers.find((s) => s.id === id);
-        if (speaker) {
-          const updatedSpeaker = {
-            ...speaker,
-            ...recordFromBody,
-          };
-          const updatedSpeakers = speakers.map((s) =>
-            s.id === id ? updatedSpeaker : s
-          );
-          await writeFile(
-            jsonFile,
-            JSON.stringify({ speakers: updatedSpeakers })
-          );
-          res.setHeader('Content-Type', 'application/json');
-          res.status(200).send(JSON.stringify(updatedSpeaker, null, 2));
-          console.log(`PUT /api/speakers/${id} status: 200`);
-        } else {
-          res.status(404).send('Not found');
-          console.log(`PUT /api/speakers/${id} status: 404`);
-        }
-      } else {
-        res.status(404).send('Not found');
-        console.log(`PUT /api/speakers/${id} status: 404`);
-      }
-    } catch (err) {
-      res.status(500).send(err);
-      console.log(`PUT /api/speakers/${id} status: 500`);
-      console.log(`Error: ${err}`);
-    }
-  };
-
-  const deleteMethod = async () => {
-    try {
-      const data = await readFile(jsonFile, 'utf8');
-      await delay(1000);
-      const speakers = JSON.parse(data).speakers;
-      if (speakers) {
-        const speaker = speakers.find((s) => s.id === id);
-        if (speaker) {
-          const updatedSpeakers = speakers.filter((s) => s.id !== id);
-          await writeFile(
-            jsonFile,
-            JSON.stringify({ speakers: updatedSpeakers })
-          );
-          res.setHeader('Content-Type', 'application/json');
-          res.status(200).send(JSON.stringify(speaker, null, 2));
-          console.log(`DELETE /api/speakers/${id} status: 200`);
-        } else {
-          res.status(404).send('Not found');
-          console.log(`DELETE /api/speakers/${id} status: 404`);
-        }
-      } else {
-        res.status(404).send('Not found');
-        console.log(`DELETE /api/speakers/${id} status: 404`);
-      }
-    } catch (err) {
-      res.status(500).send(err);
-      console.log(`DELETE /api/speakers/${id} status: 500`);
-      console.log(`Error: ${err}`);
-    }
-  };
 
   switch (method) {
     case 'POST':
-      postMethod();
+      await postMethod();
+      break;
     case 'PUT':
-      putMethod();
+      await putMethod();
+      break;
     case 'DELETE':
-      deleteMethod();
+      await deleteMethod();
+      break;
     default:
-      res.status(500).send(`Method ${method} not supported!`);
+      res.status(501).send(`Method ${method} not implemented`);
+      console.log(`Method ${method} not implemented`);
+  }
+
+  async function putMethod() {
+    try {
+      const readFileData = await readFile(jsonFile);
+      await delay(1000);
+      const speakers = JSON.parse(readFileData).speakers;
+      if (!speakers) {
+        res.status(404).send('Error: Request failed with status code 404');
+      } else {
+        const newSpeakersArray = speakers.map(function (rec) {
+          return rec.id == id ? recordFromBody : rec;
+        });
+        writeFile(
+          jsonFile,
+          JSON.stringify({ speakers: newSpeakersArray }, null, 2)
+        );
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send(JSON.stringify(recordFromBody, null, 2));
+        console.log(`PUT /api/speakers/${id}  status: 200`);
+      }
+    } catch (e) {
+      res
+        .status(500)
+        .send(`PUT /api/speakers/${id}  status: 500 unexpected error`);
+      console.log(`PUT /api/speakers/${id}  status: 200`, e);
+    }
+  }
+
+  async function deleteMethod() {
+    try {
+      const readFileData = await readFile(jsonFile);
+      await delay(1000);
+      const speakers = JSON.parse(readFileData).speakers;
+      if (!speakers) {
+        res.status(404).send('Error: Request failed with status code 404');
+      } else {
+        const newSpeakersArray = speakers.filter(function (rec) {
+          return rec.id != id;
+        });
+        writeFile(
+          jsonFile,
+          JSON.stringify({ speakers: newSpeakersArray }, null, 2)
+        );
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send(
+          JSON.stringify(
+            speakers.find((rec) => rec.id == id),
+            null,
+            2
+          )
+        );
+        console.log(`DELETE /api/speakers/${id}  status: 200`);
+      }
+    } catch (e) {
+      res
+        .status(500)
+        .send(`DELETE /api/speakers/${id}  status: 500 unexpected error`);
+      console.log(`DELETE /api/speakers/${id}  status: 200`, e);
+    }
+  }
+
+  async function postMethod() {
+    try {
+      const readFileData = await readFile(jsonFile);
+      await delay(1000);
+      const speakers = JSON.parse(readFileData).speakers;
+      if (!speakers) {
+        res.status(404).send('Error: Request failed with status code 404');
+      } else {
+        const idNew =
+          speakers.reduce((accumulator, currentValue) => {
+            const idCurrent = parseInt(currentValue.id);
+            return idCurrent > accumulator ? idCurrent : accumulator;
+          }, 0) + 1;
+
+        const newSpeakerRec = { ...recordFromBody, id: idNew.toString() };
+
+        const newSpeakersArray = [newSpeakerRec, ...speakers];
+        writeFile(
+          jsonFile,
+          JSON.stringify({ speakers: newSpeakersArray }, null, 2)
+        );
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send(JSON.stringify(newSpeakerRec, null, 2));
+        console.log(`POST /api/speakers/${id}  status: 200`);
+      }
+    } catch (e) {
+      res
+        .status(500)
+        .send(`POST /api/speakers/${id}  status: 500 unexpected error`);
+      console.log(`POST /api/speakers/${id}  status: 200`, e);
+    }
   }
 }
