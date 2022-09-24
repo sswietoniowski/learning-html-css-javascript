31db7f4d1f7d7a27cbc063315d26fd4ab5882852
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-24 11:53:53 +0200

    Added Footer component

--- projects/react/02_github_finder/src/App.js
+++ projects/react/02_github_finder/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
         <main>Content</main>
+        <Footer />
       </div>
     </Router>
   );
