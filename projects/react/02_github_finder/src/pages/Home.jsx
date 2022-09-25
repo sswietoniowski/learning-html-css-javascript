679d2baa70930fde4144050a248eebf52f85a49b
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-25 09:19:15 +0200

    Added support for env variables

--- projects/react/02_github_finder/src/pages/Home.jsx
+++ projects/react/02_github_finder/src/pages/Home.jsx
@@ -2,6 +2,8 @@ const Home = () => {
   return (
     <>
       <h1 className='text-6xl'>Welcome</h1>
+      <p>{process.env.REACT_APP_GITHUB_API_URL}</p>
+      <p>{process.env.REACT_APP_GITHUB_API_PAT}</p>
     </>
   );
 };
