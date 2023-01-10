d1934cf4147f5821cb87ae8325fd02d4027524a3
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-01-10 16:50:30 +0100

    Refactored code

--- projects/javascript/10_multimath/webpack.config.js
+++ projects/javascript/10_multimath/webpack.config.js
@@ -1,23 +1,56 @@
-module.exports = {
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+const stylesHandler = 'style-loader';
+
+const config = {
   entry: './app/app.ts',
-  devtool: 'inline-source-map',
-  mode: 'development',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'js'),
+  },
+  devServer: {
+    open: true,
+    compress: true,
+    host: 'localhost',
+  },
+  plugins: [
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+        test: /\.(ts|tsx)$/i,
+        loader: 'ts-loader',
+        exclude: ['/node_modules/'],
       },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, 'css-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts'],
   },
-  output: {
-    filename: 'bundle.js',
-  },
-  devServer: {
-    inline: false,
-  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+  }
+  return config;
 };
