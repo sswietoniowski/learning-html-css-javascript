274d04d79294cb237aa8b785ce8ecfa04b632d8f
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-01-11 18:08:12 +0100

    Code refactored to eliminate linter warnings

--- projects/react/02_github_finder/src/pages/User.jsx
+++ projects/react/02_github_finder/src/pages/User.jsx
@@ -12,6 +12,7 @@ const User = () => {
   useEffect(() => {
     getSingleUser(params.login);
     // getUserRepo(params.login)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const {
