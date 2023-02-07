a2b1af91a39e5566f30da3d42e37486f5826d527
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-02-07 15:24:45 +0100

    Tested useCallback

--- projects/react/00_sandbox/src/components/UseCallbackExample.tsx
+++ projects/react/00_sandbox/src/components/UseCallbackExample.tsx
@@ -1,5 +1,34 @@
+import React, { useState, useCallback } from 'react';
+
 const UseCallbackExample = () => {
-  return <div></div>;
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  const addTask = useCallback(() => {
+    setTasks((previousState) => [...previousState, 'New task']);
+  }, [setTasks]);
+
+  return (
+    <div>
+      <Button addTask={addTask} />
+      {tasks.map((task, index) => (
+        <p key={index}>{task}</p>
+      ))}
+    </div>
+  );
 };
+
+interface ButtonProps {
+  addTask: () => void;
+}
+
+const Button = React.memo<ButtonProps>(({ addTask }: ButtonProps) => {
+  console.log('Button rendered');
+
+  return (
+    <button onClick={addTask} className='btn btn-primary'>
+      Add task
+    </button>
+  );
+});
 
 export default UseCallbackExample;
