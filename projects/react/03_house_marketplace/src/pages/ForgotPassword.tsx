e0b7b13f06d5ce0899a10a5a5b93e6da695bd390
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-02-22 09:21:00 +0100

    Created pages and routes

---
+++ projects/react/03_house_marketplace/src/pages/ForgotPassword.tsx
@@ -0,0 +1,9 @@
+const ForgotPassword = () => {
+  return (
+    <div>
+      <h1>Forgot Password</h1>
+    </div>
+  );
+};
+
+export default ForgotPassword;
---
+++ projects/react/03_house_marketplace/src/pages/SignUp.tsx
@@ -0,0 +1,9 @@
+const SignUp = () => {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+    </div>
+  );
+};
+
+export default SignUp;
