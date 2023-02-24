92ec4bee5f4492c5174dc4943401ed33477156d0
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-02-24 13:33:50 +0100

    Added info about logged user (temporarily)

--- projects/react/03_house_marketplace/src/pages/Profile.tsx
+++ projects/react/03_house_marketplace/src/pages/Profile.tsx
@@ -1,4 +1,10 @@
+import { useEffect } from 'react';
+import { auth } from '../firebase.config';
+
 const Profile = () => {
+  useEffect(() => {
+    console.log(auth.currentUser);
+  }, []);
   return (
     <div>
       <h1>Profile</h1>
