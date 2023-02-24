57923f1899647d4173c7097617a4ee257fe66c3e
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-02-24 13:30:45 +0100

    Enabled profile page

--- projects/react/03_house_marketplace/src/App.tsx
+++ projects/react/03_house_marketplace/src/App.tsx
@@ -17,7 +17,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<SignIn />} />
+          <Route path='/profile' element={<Profile />} />
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
