b11a221620e02b08331c22079d328ecfe19e8928
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-03-14 19:03:41 +0100

    Created Explore page

--- projects/react/03_house_marketplace/src/App.tsx
+++ projects/react/03_house_marketplace/src/App.tsx
@@ -5,6 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Explore from './pages/Explore';
 import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
+import Category from './pages/Category';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
@@ -18,6 +19,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
+          <Route path='/category/:categoryName' element={<Category />} />
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
           </Route>
