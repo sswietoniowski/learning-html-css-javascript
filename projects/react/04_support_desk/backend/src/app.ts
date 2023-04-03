0b76ce76fd66c8838e58ba845fd2aae3c1254227
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-04-03 13:04:11 +0200

    Created basic configuration for Express

--- projects/react/04_support_desk/backend/src/app.ts
+++ projects/react/04_support_desk/backend/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 
 const app: Express = express();
@@ -12,6 +12,10 @@ if (import.meta.env.PROD) {
     console.log(`Server is listening on port ${port}`);
   });
 }
+
+app.get('/', (_: Request, res: Response) => {
+  res.status(200).json({ message: 'Welcome to the Support Desk API!' });
+});
 
 console.log('Express app created...');
 
