1d103d8e5b77f79f83ee51a30e0b16f39ab994c8
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-04-05 18:19:14 +0200

    Configured Redux and auth slice

--- projects/react/04_support_desk/frontend/src/app/store.ts
+++ projects/react/04_support_desk/frontend/src/app/store.ts
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../features/auth/authSlice';
 
 // Information about Redux configuration with TypeScript can be found here:
 // https://redux.js.org/usage/usage-with-typescript
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+  },
 });
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
---
+++ projects/react/04_support_desk/frontend/src/features/auth/authSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export interface AuthState {
+  user: string | null;
+  isError: boolean;
+  isSuccess: boolean;
+  isLoading: boolean;
+  message: string;
+}
+
+const initialState: AuthState = {
+user: null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {},
+});
+
+export default authSlice.reducer;
