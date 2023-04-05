e7e34f92ad59d20b1112dd0ea526136dcb86eae1
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-04-05 15:47:29 +0200

    Created Header component and Home, Login, Register pages

--- projects/react/04_support_desk/frontend/src/App.tsx
+++ projects/react/04_support_desk/frontend/src/App.tsx
@@ -1,5 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import Header from './components/Header';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+
+      <ToastContainer />
+    </>
+  );
 };
 
 export default App;
--- projects/react/04_support_desk/frontend/src/pages/Home.tsx
+++ projects/react/04_support_desk/frontend/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <div></div>;
+  return <div>Home</div>;
 };
 
 export default Home;
