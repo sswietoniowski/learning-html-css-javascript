ec244b9cd0b32a95580c16d80e92f59302af0014
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-04-13 16:48:51 +0200

    Added logging of registered routes

--- projects/react/04_support_desk/backend/src/app.ts
+++ projects/react/04_support_desk/backend/src/app.ts
@@ -6,25 +6,20 @@ import usersRouter from './routes/usersRoutes';
 import errorHandler from './middleware/errorMiddleware';
 import connectDb from './config/db';
 import ticketsRouter from './routes/ticketsRoutes';
+import helmet from 'helmet';
+import listEndpoints from 'express-list-endpoints';
 
 // Connect to database
 await connectDb();
 
 const app: Express = express();
 
+app.use(helmet()); // for security
+app.use(cors()); // for cross-origin requests
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cors()); // for cross-origin requests
 
-// @ts-ignore
-if (import.meta.env.PROD) {
-  const port: number = 3001;
-
-  app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-  });
-}
-
+// Routes
 app.get(['/', '/api'], (_: Request, res: Response) => {
   res.status(200).json({ message: 'Welcome to the Support Desk API!' });
 });
@@ -34,6 +29,24 @@ app.use('/api/tickets', ticketsRouter);
 
 app.use(errorHandler); // must be last!
 
-console.log('Express app created...'.cyan.underline);
+const endpoints = listEndpoints(app);
+const routes = endpoints.map((endpoint) => ({
+  path: endpoint.path,
+  methods: endpoint.methods.join(', '),
+}));
+
+console.log('Available endpoints:');
+routes.forEach((route) => {
+  console.log(`${route.path} [${route.methods}]`);
+});
+
+// @ts-ignore
+if (import.meta.env.PROD) {
+  const port: number = 3001;
+
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
+}
 
 export const viteNodeApp = app;
