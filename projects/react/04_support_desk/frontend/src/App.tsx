6446a9aa31ee553ee326f5b504ce1b09b885ede8
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-04-14 11:44:07 +0200

    Added stub page for a single ticket

--- projects/react/04_support_desk/frontend/src/App.tsx
+++ projects/react/04_support_desk/frontend/src/App.tsx
@@ -10,6 +10,7 @@ import Register from './pages/Register';
 import NewTicket from './pages/NewTicket';
 import Tickets from './pages/Tickets';
 import PrivateRoute from './components/PrivateRoute';
+import Ticket from './pages/Ticket';
 
 const App = () => {
   return (
@@ -26,6 +27,9 @@ const App = () => {
             </Route>
             <Route path='/tickets' element={<PrivateRoute />}>
               <Route path='/tickets' element={<Tickets />} />
+            </Route>
+            <Route path='/ticket/:id' element={<PrivateRoute />}>
+              <Route path='/ticket/:id' element={<Ticket />} />
             </Route>
           </Routes>
         </div>
---
+++ projects/react/04_support_desk/frontend/src/pages/Ticket.tsx
@@ -0,0 +1,5 @@
+const Ticket = () => {
+  return <div>Ticket</div>;
+};
+
+export default Ticket;
