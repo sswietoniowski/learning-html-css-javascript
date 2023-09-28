eec01a7a0b17cd70a0537aaf63beeeea92b8c8c1
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-09-28 08:21:15 +0200

    feat: activate husky pre-commit for this project

--- projects/react/09_jobs/lint-staged.config.js
+++ projects/react/09_jobs/lint-staged.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  '*.{ts,tsx}': [
-    'npm run lint',
-    // 'npm run types:check',
-    'npm run format:check',
-  ],
+  '*.{ts,tsx}': (files) => {
+    const isAppFolderChanges = files.some((fileName) =>
+      fileName.includes('projects/react/09_jobs')
+    );
+    const scripts = [
+      'npm run lint',
+      // 'npm run types:check',
+      'npm run format:check',
+    ];
+    return isAppFolderChanges ? scripts : [];
+  },
 };
