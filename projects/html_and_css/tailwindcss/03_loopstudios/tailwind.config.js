e91f0db430a3dcc1f97323961d48fd59246bd1ac
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-10-05 08:55:12 +0200

    feat: create Loopstudios landing page

--- projects/html_and_css/tailwindcss/03_loopstudios/tailwind.config.js
+++ projects/html_and_css/tailwindcss/03_loopstudios/tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['*'],
+  content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+        alata: ['Alata'],
+      },
+      letterSpacing: {
+        widest: '.3em',
+      },
+    },
   },
   plugins: [],
 };
