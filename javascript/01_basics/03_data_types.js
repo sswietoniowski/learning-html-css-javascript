cad6646a5cbf0a57db0f15e66a41e15aaaf8f1db
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-10-11 09:33:53 +0200

    feat: add info about different data types

--- javascript/01_basics/03_data_types.js
+++ javascript/01_basics/03_data_types.js
@@ -2,7 +2,8 @@
 // static typing -> TypeScript
 
 // Primitive data types
-// stack
+// stored on stack
+// passed by value
 
 // string
 const name = 'string';
@@ -21,22 +22,62 @@ let sex = undefined; // no value assigned
 let id = Symbol('symbol'); // unique
 
 // Reference data types
-// heap
+// stored on heap
+// passed by reference
 
-// arrays
-let numbers = [1, 2, 3];
-// object literals
+// object literals (plain objects)
 let person = {
   name: 'John',
   age: 30,
+  favoriteColors: ['red', 'blue', 'green'],
 };
+console.log(person);
+person.name = 'Bob';
 // functions
 const myFunction = () => {
   console.log('function');
 };
 // dates
 let birthDate = new Date();
+// class instances
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
 
+  sayHello() {
+    console.log(`Hello ${this.name}`);
+  }
+}
+let person1 = new Person('John');
+person1.sayHello();
+// collection types: arrays, maps, sets, weakMaps, weakSets
+let numbers = [1, 2, 3];
+let array = new Array();
+array.push('value');
+let map = new Map();
+map.set('key', 'value');
+let set = new Set();
+set.add('value');
+let weakMap = new WeakMap();
+weakMap.set(person, 'test');
+// errors
+let error = new Error('Error message');
+// promises
+let promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Promise resolved');
+  }, 1000);
+});
+promise
+  .then((value) => console.log(value))
+  .catch((error) => console.log(error));
+// newer syntax with async/await
+async function myAsyncFunction() {
+  const result = await promise;
+  console.log(result);
+}
+await myAsyncFunction();
 // Print different types
 console.log(typeof name);
 console.log(typeof age);
