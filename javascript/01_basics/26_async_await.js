eee37dddb469f687ec9419d9f82aaa85c4490917
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-10-15 09:57:49 +0200

    feat: update info about async

--- javascript/01_basics/26_async_await.js
+++ javascript/01_basics/26_async_await.js
@@ -6,3 +6,11 @@ async function myFunc() {
 
 // file deepcode ignore PromiseNotCaughtGeneral: this is just a demo code
 myFunc().then((data) => console.log(data));
+
+/*
+
+Top-level await enables modules to act as big async functions: they can await top-level promises. This is not possible in normal modules, where await can only be used in async functions.
+
+This is a new feature in ES2020 (ES11) supported by ECMAScript Modules (ESM). It is not supported in CommonJS modules.
+
+*/
