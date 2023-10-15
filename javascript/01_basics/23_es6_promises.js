2f4c363cbe6e382d2e6b837995ff0425d4f5c83c
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-10-15 10:31:10 +0200

    feat: add info about concurrent promises

--- javascript/01_basics/23_es6_promises.js
+++ javascript/01_basics/23_es6_promises.js
@@ -33,3 +33,18 @@ createPost({ title: 'Post 3', body: 'This is post 3' })
   .catch((error) => {
     console.log(error);
   });
+
+// Concurrent promises
+
+const promise1 = Promise.resolve('Hello World');
+const promise2 = 10;
+const promise3 = new Promise(function (resolve, reject) {
+  setTimeout(resolve, 2000, 'Goodbye');
+});
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(
+  (res) => res.json()
+);
+
+Promise.all([promise1, promise2, promise3, promise4]).then((values) =>
+  console.log(values)
+);
