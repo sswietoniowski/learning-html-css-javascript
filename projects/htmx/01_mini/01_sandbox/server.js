d97d052a1b5fa2cf8f13aa76b7a37d295d50663a
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2024-03-25 21:21:41 +0100

    fix: security vulnerabilities

--- projects/htmx/01_mini/01_sandbox/server.js
+++ projects/htmx/01_mini/01_sandbox/server.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import xss from 'xss';
 import dotenv from 'dotenv';
+import csurf from 'csurf';
+import helmet from 'helmet';
 
 const app = express();
+
+app.use(csurf());
+app.use(helmet({ hidePoweredBy: true }));
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
